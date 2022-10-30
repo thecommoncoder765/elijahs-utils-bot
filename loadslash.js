430d131e56880a750b27681e84b71fc1f46df75e
thecommoncoder7 <65053816+thecommoncoder765@users.noreply.github.com>
2022-10-30 23:13:25 +0000

    Added guildId and guild get, and error function to loadslash.js

--- loadslash.js
+++ loadslash.js
@@ -10,14 +10,18 @@ let bot = {
   client
 }
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}`)
-})
+const guildId = "1034240157815156776"
 
 client.slashcommands = new Discord.Collection()
 
 client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
 client.loadSlashCommands(bot, false)
+
+client.on("ready", () => {
+  const guild = client.guilds.cache.get(guildId)
+  if (!guild)
+    return console.error("Target guild not found")
+})
 
 const token = process.env['TOKEN']
 client.login(token)
