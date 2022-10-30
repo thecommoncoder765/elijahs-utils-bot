9a8db9c8c6f41e4f3da48c08d4169f612efaa5c0
thecommoncoder7 <65053816+thecommoncoder765@users.noreply.github.com>
2022-10-30 23:19:52 +0000

    Changed the client-on ready-function to async

--- loadslash.js
+++ loadslash.js
@@ -17,10 +17,12 @@ client.slashcommands = new Discord.Collection()
 client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
 client.loadSlashCommands(bot, false)
 
-client.on("ready", () => {
+client.on("ready", async () => {
   const guild = client.guilds.cache.get(guildId)
   if (!guild)
     return console.error("Target guild not found")
+
+  await guild.commands.set([...client.slashcommands.values()])
 })
 
 const token = process.env['TOKEN']
