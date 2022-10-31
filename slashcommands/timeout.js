91508aa38cdee8190968e5161c086cefbc11c9d3
thecommoncoder7 <65053816+thecommoncoder765@users.noreply.github.com>
2022-10-31 15:46:45 +0000

    Finalized timeout command

---
+++ slashcommands/timeout.js
@@ -0,0 +1,55 @@
+const durations = [
+  {name: "10 secconds", value: 10 * 1000},
+  {name: "60 secconds", value: 60 * 1000},
+  {name: "5 minutes", value: 5 * 60 * 1000},
+  {name: "10 minutes", value: 10 * 60 * 1000},
+  {name: "30 minutes", value: 30 * 60 * 1000},
+  {name: "1 hour", value: 60 * 60 * 1000},
+  {name: "1 day", value: 24 * 60 * 60 * 1000},
+  {name: "1 week", value: 7 * 24 * 60 * 60 * 1000},
+]
+const run = async (client, interaction) => {
+  let member = interactions.options.getMember("user")
+  let duration = interactions.options.getNumber("duration")
+  let reason = interactions.options.getString("reason") || "No reason given"
+
+  if (!member) return interaction.reply("Invalid member")
+
+  try {
+    await member.timeout(duration, reason)
+    return interaction.reply(`${member.user.tag} has been timed out for ${durations.find(d=> duration === d.value)?.name} with a reason of ${reason}`)
+  }
+  catch (err){
+    if (err){
+      console.error(err)
+      return interaction.reply(`Failed to timeout ${member.user.tag}`)
+    }
+  }
+}
+
+module.exports = {
+  name: "timeout",
+  description: "This is the timeout command for unruly members :)",
+  perm: "MODERATE_MEMBERS",
+  options: [
+    {
+      name: "user",
+      description: "The user to timeout",
+      type: "USER",
+      required: true
+    },
+    {
+      name: "duration",
+      description: "Duration you would like to time out a member",
+      type: "NUMBER",
+      choices: durations,
+      required: true
+    },
+    {
+      name: "reason",
+      description: "Reason for timeout",
+      type: "STRING",
+      required: false
+    }
+  ]
+}
